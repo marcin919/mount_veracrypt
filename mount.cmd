93d4b8e357eb960d38804fa35df94c539c83d512
marcin919 <mar.szmyd@gmail.com>
2020-02-25 14:23:51 +0100

    Update mount.cmd

--- mount.cmd
+++ mount.cmd
@@ -1,19 +1,18 @@
-:: VeraCrypt Mount
+:: VeraCrypt Mouting Script
 
-
-set TRUECRYPT=VeraCryptPortable.exe
-set CONTAINER=[Container-JR1.5G].vc
+set EXE=VeraCryptPortable.exe
+set CONTAINER=Container.vc
 set LETTER=S:
-if not exist "%TRUECRYPT%" (
-  echo "%TRUECRYPT%" doesn't exist
+
+if not exist "%EXE%" (
+	echo "%EXE%" doesn't exist
   pause
 )else if exist %LETTER% (
-  echo %LETTER% is already in use
+	echo %LETTER% is already in use
   pause
 ) else if not exist "%CONTAINER%" (
-  echo "%CONTAINER%" doesn't exist
+	echo "%CONTAINER%" doesn't exist
   pause
 ) else if exist "%CONTAINER%" (
- REM "%TRUECRYPT%" "%CONTAINER%" /l %LETTER% /a /q
-"%TRUECRYPT%"  /v "%CONTAINER%" /l %LETTER% /a  /e /b
+"%EXE%"  /v "%CONTAINER%" /l %LETTER% /a  /e /b
 )
